ef7157a9ac63c5925fb9dc7114e0ebcdc48bf2de
llsonyll <sony_s07@hotmail.es>
2022-08-26 16:01:06 -0500

    feat: searchbar by name or episode number

--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="episodes">
     <h3 class="text-2xl font-bold mb-4">Breaking Bad Episodes</h3>
-
     <div class="list" v-if="!error">
       <div class="loading my-10" v-if="loading">Loading...</div>
       <div class="content" v-else>
-        <div class="empty" v-if="episodes.length === 0">No episodes</div>
+        <form class="searchbar my-4" @submit="handleSearch">
+          <input
+            class="px-3 py-1 w-full"
+            type="text"
+            :value="search"
+            @input="(event) => (search = event.target.value)"
+            placeholder="Episode number or name"
+          />
+        </form>
+
+        <div class="empty" v-if="searchResults.length === 0">No episodes</div>
         <div class="with-content" v-else>
-          <EpisodeCard v-for="ep in episodes" :episode="ep" />
+          <EpisodeCard v-for="ep in searchResults" :episode="ep" />
         </div>
       </div>
     </div>
@@ -32,10 +41,27 @@ export default {
       episodes: [],
       loading: true,
       error: null,
+      search: "",
     };
   },
   created() {
     this.getEpisodes();
+  },
+  computed: {
+    searchResults() {
+      if (!this.search) return this.episodes;
+      if (Number(this.search)) {
+        const index = this.episodes.findIndex(
+          (ep) => ep.episode_id === Number(this.search)
+        );
+        return index !== -1 ? [this.episodes[index]] : [];
+      } else {
+        const results = this.episodes.filter((ep) => {
+          return ep.title.toLowerCase().includes(this.search);
+        });
+        return results;
+      }
+    },
   },
   methods: {
     async getEpisodes() {
@@ -51,6 +77,10 @@ export default {
         this.error = "Error de red";
         this.loading = false;
       }
+    },
+    handleSearch(e) {
+      e.preventDefault();
+      console.log(this.search);
     },
   },
 };
